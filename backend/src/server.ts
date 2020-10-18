54374925c516748653e29b9309731bbd57f6ab56
Josimar <josimarjosesousa@gmail.com>
2020-10-18 13:35:35 -0300

    arquivos pack.json, server.ts, yarn.lock

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/users', (request, response) =>{
-    console.log('teste');
+   
+    return response.json({ message: 'Hello World'});
 });
 
 app.listen(3333);
+
