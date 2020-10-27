60d06fc78d3362e1a036ba4239aa24bcbef36a59
Josimar <josimarjosesousa@gmail.com>
2020-10-26 21:23:32 -0300

    criacao migration

---
+++ backend/src/database/migrations/1603758138952-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1603758138952 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
