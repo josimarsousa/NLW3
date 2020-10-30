b4ab63ab5f335c2ebc4b45d04fc9afcdab48bdd6
Josimar <josimarjosesousa@gmail.com>
2020-10-30 06:49:49 -0300

    mapeamento de imagens pronto

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -24,6 +24,7 @@ export default{
     },
 
     async create(request: Request, response: Response){
+
         const {
             name, 
             latitude,
@@ -35,6 +36,11 @@ export default{
         } = request.body;
     
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(images => {
+            return { path: images.filename }
+        })
     
         const orphanage = orphanagesRepository.create({
             name, 
@@ -44,6 +50,7 @@ export default{
             instructions,
             opening_hours,
             open_on_weekends,
+            images
         });
     
         
