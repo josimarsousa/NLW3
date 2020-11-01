cd511453d6287fd7ad41d982a8e46fce0ab5609e
Josimar <josimarjosesousa@gmail.com>
2020-11-01 07:55:28 -0300

    backend concluido

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
+
 import 'express-async-errors';
 
 import './database/connection';
@@ -12,6 +14,7 @@ import './database/connection';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
